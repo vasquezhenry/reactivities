fe7a6ee7a2b9d8e3acc885638907c7d279b1ac1f
Henry Vasquez <henry.vasquez@nrg-edge.com>
2021-04-22 16:52:03 -0400

    activity dashboard basic ui complete

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+import ActivityList from './ActivityList';
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  createOrEditActivity: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export default function ActivityDashboard({
+  activities,
+  selectActivity,
+  cancelSelectActivity,
+  selectedActivity,
+  editMode,
+  openForm,
+  closeForm,
+  createOrEditActivity,
+  deleteActivity,
+}: Props) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && (
+          <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEditActivity={createOrEditActivity} />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+}
